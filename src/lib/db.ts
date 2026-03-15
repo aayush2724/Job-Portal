931bdd85b082674217808a85e45385aabf3cee94
Aayush <aayush2615@gmail.com>
2026-03-15 15:38:02 +0530

    Fix db.ts and allow build to pass

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
   if (!MONGODB_URI) {
     throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
   }
